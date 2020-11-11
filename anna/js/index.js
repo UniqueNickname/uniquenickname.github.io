da7bb9681cf397b4ec87714804d6202bb9e59a61
Arm-1606 <74306084+Arm-1606@users.noreply.github.com>
2020-11-11 21:55:06 +0600

    added not working touch DnD

--- anna/js/index.js
+++ anna/js/index.js
@@ -1,282 +1,347 @@
 window.onload = () => {
-  const teachers = [
-    {
-      id: 1,
-      text: 'Батенькина Оксана Васильевна'
-    },
-    {
-      id: 2,
-      text: 'Большакова Мария Сергеевна'
-    },
-    {
-      id: 3,
-      text: 'Голунов Александр Владимирович'
-    },
-    {
-      id: 4,
-      text: 'Котюргина Александра Станиславовна'
-    },
-    {
-      id: 5,
-      text: 'Макарова Таисья Васильевна'
-    },
-    {
-      id: 6,
-      text: 'Ровкина Вероника Дмитриевна'
-    },
-    {
-      id: 7,
-      text: 'Степаненко Алексей Николаевич'
-    },
-    {
-      id: 8,
-      text: 'Тиховская Светлана Валерьевна'
-    },
-    {
-      id: 9,
-      text: 'Чемерилов Александр Сергеевич'
-    },
-    {
-      id: 10,
-      text: 'Шевелева Ольга Геннадьевна'
-    },
-    {
-      id: 11,
-      text: 'Юдин Евгений Борисович'
-    }
-  ]
+	const teachers = [
+		{
+			id: 1,
+			text: 'Батенькина Оксана Васильевна'
+		},
+		{
+			id: 2,
+			text: 'Большакова Мария Сергеевна'
+		},
+		{
+			id: 3,
+			text: 'Голунов Александр Владимирович'
+		},
+		{
+			id: 4,
+			text: 'Котюргина Александра Станиславовна'
+		},
+		{
+			id: 5,
+			text: 'Макарова Таисья Васильевна'
+		},
+		{
+			id: 6,
+			text: 'Ровкина Вероника Дмитриевна'
+		},
+		{
+			id: 7,
+			text: 'Степаненко Алексей Николаевич'
+		},
+		{
+			id: 8,
+			text: 'Тиховская Светлана Валерьевна'
+		},
+		{
+			id: 9,
+			text: 'Чемерилов Александр Сергеевич'
+		},
+		{
+			id: 10,
+			text: 'Шевелева Ольга Геннадьевна'
+		},
+		{
+			id: 11,
+			text: 'Юдин Евгений Борисович'
+		}
+	]
 
-  const subjects = [
-    {
-      id: 1,
-      text: 'Технологии виртуальной реальности'
-    },
-    {
-      id: 2,
-      text: 'Дизайн интерфейса информационных систем'
-    },
-    {
-      id: 3,
-      text: 'Информационные технологии в PreMedia'
-    },
-    {
-      id: 4,
-      text: 'Системный анализ'
-    },
-    {
-      id: 5,
-      text: 'Технологии создания гипертекстовых систем'
-    },
-    {
-      id: 6,
-      text: 'Управление информационными проектами и ресурсами'
-    },
-    {
-      id: 7,
-      text: 'Трехмерное компьютерное моделирование'
-    },
-    {
-      id: 8,
-      text: 'Интеллектуальные системы в медиаиндустрии'
-    },
-    {
-      id: 9,
-      text: 'Технологии анимации'
-    },
-    {
-      id: 10,
-      text: 'Проектная деятельность'
-    },
-    {
-      id: 11,
-      text: 'Объектно-ориентированное программирование'
-    }
-  ]
+	const subjects = [
+		{
+			id: 1,
+			text: 'Технологии виртуальной реальности'
+		},
+		{
+			id: 2,
+			text: 'Дизайн интерфейса информационных систем'
+		},
+		{
+			id: 3,
+			text: 'Информационные технологии в PreMedia'
+		},
+		{
+			id: 4,
+			text: 'Системный анализ'
+		},
+		{
+			id: 5,
+			text: 'Технологии создания гипертекстовых систем'
+		},
+		{
+			id: 6,
+			text: 'Управление информационными проектами и ресурсами'
+		},
+		{
+			id: 7,
+			text: 'Трехмерное компьютерное моделирование'
+		},
+		{
+			id: 8,
+			text: 'Интеллектуальные системы в медиаиндустрии'
+		},
+		{
+			id: 9,
+			text: 'Технологии анимации'
+		},
+		{
+			id: 10,
+			text: 'Проектная деятельность'
+		},
+		{
+			id: 11,
+			text: 'Объектно-ориентированное программирование'
+		}
+	]
 
-  const positions = [
-    {
-      id: 1,
-      text: 'доцент, кандидат технических наук'
-    },
-    {
-      id: 2,
-      text: 'ассистент'
-    },
-    {
-      id: 3,
-      text: 'доцент, кандидат технических наук'
-    },
-    {
-      id: 4,
-      text: 'доцент, кандидат технических наук'
-    },
-    {
-      id: 5,
-      text: 'доцент, кандидат педагогических наук'
-    },
-    {
-      id: 6,
-      text: 'старший преподаватель'
-    },
-    {
-      id: 7,
-      text: 'старший преподаватель'
-    },
-    {
-      id: 8,
-      text: 'доцент, кандидат физико-математических наук'
-    },
-    {
-      id: 9,
-      text: 'ассистент'
-    },
-    {
-      id: 10,
-      text: 'старший преподаватель'
-    },
-    {
-      id: 11,
-      text: 'доцент, кандидат технических наук'
-    }
-  ]
+	const positions = [
+		{
+			id: 1,
+			text: 'доцент, кандидат технических наук'
+		},
+		{
+			id: 2,
+			text: 'ассистент'
+		},
+		{
+			id: 3,
+			text: 'доцент, кандидат технических наук'
+		},
+		{
+			id: 4,
+			text: 'доцент, кандидат технических наук'
+		},
+		{
+			id: 5,
+			text: 'доцент, кандидат педагогических наук'
+		},
+		{
+			id: 6,
+			text: 'старший преподаватель'
+		},
+		{
+			id: 7,
+			text: 'старший преподаватель'
+		},
+		{
+			id: 8,
+			text: 'доцент, кандидат физико-математических наук'
+		},
+		{
+			id: 9,
+			text: 'ассистент'
+		},
+		{
+			id: 10,
+			text: 'старший преподаватель'
+		},
+		{
+			id: 11,
+			text: 'доцент, кандидат технических наук'
+		}
+	]
 
-  const store = {}
+	const store = {}
 
-  const DnD = {
-    drag: {
-      over: event => {
-        if (isNotSuccess(event.target)) event.preventDefault();
-      },
-      start: (event) => {
-        const $item = event.target
-        setTimeout(() => {
-          if ($item.style) $item.style.display = 'none'
-        }, 0)
-      },
-      end: event => {
-        const $item = event.target //? блок, который мы переместили
-        if ($item.style) $item.style.display = 'block'
+	const touch = {
+		start: event => {
+			const $item = event.target
+			if (isNotSuccess($item)) {
+				store.defaultY = $item.offsetTop
+				store.defaultX = $item.offsetLeft
+				store.differentY = event.targetTouches[0].pageY - store.defaultY
+				store.differentX = event.targetTouches[0].pageX - store.defaultX
+				store.itemsPosition = getItemsPositions()
+			}
+		},
+		move: event => {
+			const $item = event.target
+			if (isNotSuccess($item)) {
+				store.lastY = event.targetTouches[0].pageY
+				store.lastX = event.targetTouches[0].pageX
+				const { differentY, differentX, lastY, lastX } = store
+				const coordY = lastY - differentY
+				const coordX = lastX - differentX
+				$item.style.top = coordY + 'px'
+				$item.style.left = coordX + 'px'
+			}
+		},
+		end: event => {
+			const $item = event.target //? блок, который мы переместили
+			if (isNotSuccess($item)) {
+				const { defaultY, defaultX, lastY, lastX } = store
+				let blockMoved = false
+				store.itemsPosition.forEach((size, index) => {
+					if (lastY > size.top
+						&& lastY < size.bottom
+						&& lastX > size.left
+						&& lastX < size.right) {
+						const $oldItem = store.$items[index] //? блок, который находился в ячейке, в которую мы переместили новый блок
+						blockMoved = changePlacesOfItems($item, $oldItem)
+						blockMoved = true
+					}
+				})
+				if (!blockMoved) {
+					$item.style.top = defaultY + 'px'
+					$item.style.left = defaultX + 'px'
+				}
+			}
+		}
+	}
 
-        const { $oldItem } = store //? блок, который находился в ячейке, в которую мы переместили новый блок
-        if ($oldItem) {
-          const $newCell = $oldItem.parentElement //? ячейка, из которой мы взяли блок
-          const $oldCell = $item.parentElement //? ячейка, в которую мы переместили блок
+	const DnD = {
+		drag: {
+			over: event => {
+				if (isNotSuccess(event.target)) event.preventDefault();
+			},
+			start: (event) => {
+				const $item = event.target
+				setTimeout(() => {
+					if ($item.style) $item.style.display = 'none'
+				}, 0)
+			},
+			end: event => {
+				const $item = event.target //? блок, который мы переместили
+				if ($item.style) $item.style.display = 'block'
 
-          const oldCol = $oldCell.dataset.col
-          const newCol = $newCell.dataset.col
+				const { $oldItem } = store //? блок, который находился в ячейке, в которую мы переместили новый блок
+				if ($oldItem) {
+					changePlacesOfItems($item, $oldItem)
+				}
+			}
+		},
+		drop: event => {
+			store.$oldItem = event.target
+		}
+	}
 
-          if (oldCol === newCol && isNotSuccess($item)) {
-            $newCell.append($item)
-            $oldCell.append($oldItem)
+	const changePlacesOfItems = ($item, $oldItem) => {
+		const $newCell = $oldItem.parentElement //? ячейка, из которой мы взяли блок
+		const $oldCell = $item.parentElement //? ячейка, в которую мы переместили блок
 
-            const $newLine = $newCell.parentElement
-            const $oldLine = $oldCell.parentElement
+		console.log({ $newCell, $item, $oldItem, $oldCell })
+		const oldCol = $oldCell.dataset.col
+		const newCol = $newCell.dataset.col
+		if (oldCol === newCol && isNotSuccess($item)) {
+			$newCell.append($item)
+			$oldCell.append($oldItem)
 
-            const newLineId = getId($newLine.querySelectorAll('.item'))
-            const oldLineId = getId($oldLine.querySelectorAll('.item'))
+			const $newLine = $newCell.parentElement
+			const $oldLine = $oldCell.parentElement
 
-            compareId($newLine, newLineId)
-            compareId($oldLine, oldLineId)
-          }
-        }
-      }
-    },
-    drop: event => {
-      store.$oldItem = event.target
-    }
-  }
+			const newLineId = getId($newLine.querySelectorAll('.item'))
+			const oldLineId = getId($oldLine.querySelectorAll('.item'))
 
-  const isNotSuccess = $el => !$el.parentElement.parentElement.classList.contains('success')
+			compareId($newLine, newLineId)
+			compareId($oldLine, oldLineId)
+			return true
+		} else return false
+	}
 
-  const compareId = ($line, id) => {
-    if (id[0] === id[1]) {
-      if (id[0] === id[2]) {
-        $line.classList.add('success')
-        disableDraggable($line)
-      } else {
-        const currentPosition = positions.find(position => position.id == id[2]).text
-        const samePositions = positions.filter(position => position.text === currentPosition)
-        samePositions.forEach(position => {
-          if (position.id == id[0]) {
-            $line.classList.add('success')
-            disableDraggable($line)
-          }
-        })
-      }
-    }
-  }
+	const getItemsPositions = () => {
+		return store.$items.map($item => ({
+			top: $item.offsetTop,
+			left: $item.offsetLeft,
+			right: $item.offsetLeft + $item.clientWidth,
+			bottom: $item.offsetTop + $item.clientHeight
+		}))
+	}
 
-  const disableDraggable = $line => {
-    const $items = $line.querySelectorAll('.item')
-    $items.forEach($item => $item.setAttribute('draggable', false))
-  }
+	const isNotSuccess = $el => !$el.parentElement.parentElement.classList.contains('success')
 
-  const getId = $elems => [...$elems].map($el => $el.dataset.id)
+	const compareId = ($line, id) => {
+		if (id[0] === id[1]) {
+			if (id[0] === id[2]) {
+				$line.classList.add('success')
+				disableDraggable($line)
+			} else {
+				const currentPosition = positions.find(position => position.id == id[2]).text
+				const samePositions = positions.filter(position => position.text === currentPosition)
+				samePositions.forEach(position => {
+					if (position.id == id[0]) {
+						$line.classList.add('success')
+						disableDraggable($line)
+					}
+				})
+			}
+		}
+	}
 
-  const randomInt = max => {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+	const disableDraggable = $line => {
+		const $items = $line.querySelectorAll('.item')
+		$items.forEach($item => $item.setAttribute('draggable', false))
+	}
 
-  const createCell = ($line, { id, text }, colNumber) => {
-    const $cell = document.createElement('div');
-    const $item = document.createElement('div');
-    const $text = document.createTextNode(text)
-    $cell.classList.add('cell')
-    $item.classList.add('item')
-    $item.setAttribute('data-id', id)
-    $cell.setAttribute('data-col', colNumber)
-    $item.setAttribute('draggable', true)
-    $line.append($cell)
-    $cell.append($item)
-    $item.append($text)
-    $cell.ondragover = DnD.drag.over
-    $cell.ondrop = DnD.drop
-    $item.ondragstart = DnD.drag.start
-    $item.ondragend = DnD.drag.end
-  }
+	const getId = $elems => [...$elems].map($el => $el.dataset.id)
 
-  const render = linesNumber => {
-    const $container = document.querySelector('.container')
-    const $lines = []
-    for (let index = 0; index < linesNumber; index++) {
-      const $line = document.createElement('div');
-      $line.classList.add('line')
-      $container.append($line)
-      $lines.push($line)
-    }
-    const usedTeachers = []
-    const usedSubjects = []
-    const usedPositions = []
-    $lines.forEach(($line, lineIndex) => {
-      let randomTeachersIndex;
-      let randomSubjectsIndex;
-      let randomPositionsIndex;
+	const randomInt = max => {
+		return Math.floor(Math.random() * Math.floor(max));
+	}
 
-      do {
-        randomTeachersIndex = randomInt(linesNumber)
-      } while (usedTeachers.includes(randomTeachersIndex))
-      usedTeachers.push(randomTeachersIndex)
+	const createCell = ($line, { id, text }, colNumber) => {
+		const $cell = document.createElement('div');
+		const $item = document.createElement('div');
+		const $text = document.createTextNode(text)
+		$cell.classList.add('cell')
+		$item.classList.add('item')
+		$item.setAttribute('data-id', id)
+		$cell.setAttribute('data-col', colNumber)
+		$item.setAttribute('draggable', true)
+		$line.append($cell)
+		$cell.append($item)
+		$item.append($text)
+		$cell.ondragover = DnD.drag.over
+		$cell.ondrop = DnD.drop
+		$item.ondragstart = DnD.drag.start
+		$item.ondragend = DnD.drag.end
+		$item.ontouchstart = touch.start
+		$item.ontouchmove = touch.move
+		$item.ontouchend = touch.end
+		if (!store.$items) store.$items = []
+		store.$items.push($item)
+	}
 
-      do {
-        randomSubjectsIndex = randomInt(linesNumber)
-      } while (
-        usedTeachers[lineIndex] === randomSubjectsIndex
-        || usedSubjects.includes(randomSubjectsIndex))
-      usedSubjects.push(randomSubjectsIndex)
+	const render = linesNumber => {
+		const $container = document.querySelector('.container')
+		const $lines = []
+		for (let index = 0; index < linesNumber; index++) {
+			const $line = document.createElement('div');
+			$line.classList.add('line')
+			$container.append($line)
+			$lines.push($line)
+		}
+		const usedTeachers = []
+		const usedSubjects = []
+		const usedPositions = []
+		$lines.forEach(($line, lineIndex) => {
+			let randomTeachersIndex;
+			let randomSubjectsIndex;
+			let randomPositionsIndex;
 
-      do {
-        randomPositionsIndex = randomInt(linesNumber)
-      } while (
-        usedTeachers[lineIndex] === randomPositionsIndex
-        || usedSubjects[lineIndex] === randomPositionsIndex
-        || usedPositions.includes(randomPositionsIndex))
-      usedPositions.push(randomPositionsIndex)
+			do {
+				randomTeachersIndex = randomInt(linesNumber)
+			} while (usedTeachers.includes(randomTeachersIndex))
+			usedTeachers.push(randomTeachersIndex)
 
-      createCell($line, teachers[randomTeachersIndex], 1)
-      createCell($line, subjects[randomSubjectsIndex], 2)
-      createCell($line, positions[randomPositionsIndex], 3)
-    })
-  }
+			do {
+				randomSubjectsIndex = randomInt(linesNumber)
+			} while (
+				usedTeachers[lineIndex] === randomSubjectsIndex
+				|| usedSubjects.includes(randomSubjectsIndex))
+			usedSubjects.push(randomSubjectsIndex)
 
-  render(11)
+			do {
+				randomPositionsIndex = randomInt(linesNumber)
+			} while (
+				usedTeachers[lineIndex] === randomPositionsIndex
+				|| usedSubjects[lineIndex] === randomPositionsIndex
+				|| usedPositions.includes(randomPositionsIndex))
+			usedPositions.push(randomPositionsIndex)
+
+			createCell($line, teachers[randomTeachersIndex], 1)
+			createCell($line, subjects[randomSubjectsIndex], 2)
+			createCell($line, positions[randomPositionsIndex], 3)
+		})
+	}
+
+	render(11)
 }
