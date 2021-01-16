b2eea6925b46967dd30dae5b95f3d5b18a34f728
Unique <dr.art-47@yandex.ru>
2021-01-16 21:38:32 +0600

    cartButtons

---
+++ polifood_3/assets/js/catalog.js
@@ -0,0 +1,12 @@
+// products__card-buy
+const $cartButtons = document.querySelectorAll(".products__card-buy");
+
+$cartButtons.forEach(($button) => {
+    $button.addEventListener("click", (event) => {
+        event.preventDefault();
+        console.log(window.location.href.split("/").slice(0, -3).join("/"));
+        window.location.href =
+            window.location.href.split("/").slice(0, -3).join("/") +
+            "/cart/index.html";
+    });
+});
